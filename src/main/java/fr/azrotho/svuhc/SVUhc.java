d886f61d021163a5fa44feb04acab1cf303744b9
Azrotho <azrotho.yt@gmail.com>
2024-02-05 22:28:29 +0100

    Ajouter la commande "nick" pour gérer les pseudonymes des joueurs, édition du pom.xml, rework soon du NickManager.

--- src/main/java/fr/azrotho/svuhc/SVUhc.java
+++ src/main/java/fr/azrotho/svuhc/SVUhc.java
@@ -4,19 +4,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.iridium.iridiumcolorapi.IridiumColorAPI;
 
+import fr.azrotho.svuhc.commands.NickCommand;
 import fr.azrotho.svuhc.commands.TeamCommand;
 import fr.azrotho.svuhc.objects.SVPlayers;
+import fr.azrotho.svuhc.utils.NickManager;
 
 public class SVUhc extends JavaPlugin {
     private static SVUhc INSTANCE;
     private SVPlayers players;
     private String tag = "§f§l[" + IridiumColorAPI.process("<GRADIENT:2C08BA>§lSaint-Valentin UHC</GRADIENT:028A97>") + "§f§l] §e§l➤ ";
+    private NickManager nickManager;
 
     @Override
     public void onEnable() {
         INSTANCE = this;
         getCommand("team").setExecutor(new TeamCommand());
-        
+        getCommand("nick").setExecutor(new NickCommand());
+        nickManager = new NickManager();
     }
 
     @Override
@@ -34,5 +38,9 @@ public class SVUhc extends JavaPlugin {
 
     public String getTag() {
         return this.tag;
+    }
+
+    public NickManager getNickManager() {
+        return this.nickManager;
     }
 }
