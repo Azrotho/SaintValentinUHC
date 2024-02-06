7f55fa75456173e3175d7fb996683e00486a0e7a
Azrotho <azrotho.yt@gmail.com>
2024-02-06 18:25:37 +0100

    fixes lol

--- src/main/java/fr/azrotho/svuhc/SVUhc.java
+++ src/main/java/fr/azrotho/svuhc/SVUhc.java
@@ -4,24 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.iridium.iridiumcolorapi.IridiumColorAPI;
 
-import fr.azrotho.svuhc.commands.NickCommand;
 import fr.azrotho.svuhc.commands.TeamCommand;
 import fr.azrotho.svuhc.objects.SVPlayers;
-import fr.azrotho.svuhc.utils.PlayerNameChanger;
 
 public class SVUhc extends JavaPlugin {
     private static SVUhc INSTANCE;
     private SVPlayers players;
     private String tag = "§f§l[" + IridiumColorAPI.process("<GRADIENT:2C08BA>§lSaint-Valentin UHC</GRADIENT:028A97>") + "§f§l] §e§l➤ ";
-    private PlayerNameChanger nameChanger;
 
     @Override
     public void onEnable() {
         INSTANCE = this;
         getCommand("team").setExecutor(new TeamCommand());
-        getCommand("nick").setExecutor(new NickCommand());
         this.players = new SVPlayers();
-        this.nameChanger = new PlayerNameChanger();
     }
 
     @Override
@@ -39,9 +34,5 @@ public class SVUhc extends JavaPlugin {
 
     public String getTag() {
         return this.tag;
-    }
-
-    public PlayerNameChanger getNameChanger() {
-        return this.nameChanger;
     }
 }
