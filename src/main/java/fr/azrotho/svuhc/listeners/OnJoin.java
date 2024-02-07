efbb64b5814ba6ed29381a6be535babc7e09afbc
Azrotho <azrotho.yt@gmail.com>
2024-02-07 18:30:47 +0100

    Ajouter une fonctionnalité de téléportation et de création de boîte pour les nouveaux joueurs lorsqu'ils rejoignent le serveur

--- src/main/java/fr/azrotho/svuhc/listeners/OnJoin.java
+++ src/main/java/fr/azrotho/svuhc/listeners/OnJoin.java
@@ -1,5 +1,8 @@
 package fr.azrotho.svuhc.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -13,5 +16,24 @@ public class OnJoin implements Listener {
         SVUhc.getInstance().players().addPlayer(event.getPlayer());
         if(event.getPlayer().hasPlayedBefore()) return;
         event.getPlayer().sendMessage(SVUhc.getInstance().getTag() + "§cBienvenue sur le Serveur !");
+        Player player = event.getPlayer();
+        if(!SVUhc.getInstance().isStarted()) {
+            player.teleport(new org.bukkit.Location(Bukkit.getWorld("world"), 0, 196, 0));
+            // Create box at 0,0
+            for (int x = -5; x < 5; x++) {
+                for (int y = 195; y < 200; y++) {
+                    for (int z = -5; z < 5; z++) {
+                        player.getWorld().getBlockAt(x, y, z).setType(Material.BARRIER);
+                    }
+                }
+            }
+            for (int x = -4; x < 4; x++) {
+                for (int y = 196; y < 199; y++) {
+                    for (int z = -4; z < 4; z++) {
+                        player.getWorld().getBlockAt(x, y, z).setType(Material.AIR);
+                    }
+                }
+            }
+        }
     }
 }
