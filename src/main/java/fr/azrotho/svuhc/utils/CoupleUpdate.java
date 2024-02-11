93ca331bd11e76df264cffc04c3f45ae75dd610d
Azrotho <azrotho.yt@gmail.com>
2024-02-11 11:52:27 +0100

    Ajout de la méthode update dans CoupleUpdate

--- src/main/java/fr/azrotho/svuhc/utils/CoupleUpdate.java
+++ src/main/java/fr/azrotho/svuhc/utils/CoupleUpdate.java
@@ -1,5 +1,9 @@
 package fr.azrotho.svuhc.utils;
 
+import fr.azrotho.svuhc.objects.SVCouple;
+
 public class CoupleUpdate {
-    
+    public static void update(SVCouple couple) {
+        
+    }
 }
