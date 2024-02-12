f5a0c416f320e116691454e1efb2130c7df41f93
Azrotho <azrotho.yt@gmail.com>
2024-02-12 20:41:42 +0100

    Ajout du fichier TrackingRunnable.java

---
+++ src/main/java/fr/azrotho/svuhc/runnable/TrackingRunnable.java
@@ -0,0 +1,12 @@
+package fr.azrotho.svuhc.runnable;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class TrackingRunnable extends BukkitRunnable {
+
+    @Override
+    public void run() {
+        
+    }
+    
+}
