ab910572ed7250062962ff607803699735c08320
Azrotho <contact@azrotho.fr>
2024-02-13 10:44:01 +0100

    Ajouter les commandes et class Bisou, ColleSerre et UWU (à implémenter)

---
+++ src/main/java/fr/azrotho/svuhc/commands/UWUCommand.java
@@ -0,0 +1,17 @@
+package fr.azrotho.svuhc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class UWUCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String name,
+            @NotNull String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onCommand'");
+    }
+    
+}
